9ddd1b179702564b27c3aa7a528a8cdc87e8996c
Hunseong <phunseong@gmail.com>
2022-04-04 05:00:25 +0900

    Filter 등록을 통한 로그 출력

---
+++ login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,24 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+/**
+ * Created by Hunseong on 2022/04/04
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        return filterFilterRegistrationBean;
+    }
+}
