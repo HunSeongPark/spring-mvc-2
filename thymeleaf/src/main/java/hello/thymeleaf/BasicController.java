f231533baf73e6de831e7d415a438a29ce439097
Hunseong <phunseong@gmail.com>
2022-04-03 17:14:45 +0900

    thymeleaf-text

---
+++ thymeleaf/src/main/java/hello/thymeleaf/BasicController.java
@@ -0,0 +1,27 @@
+package hello.thymeleaf;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Hunseong on 2022/04/03
+ */
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
+    }
+}
