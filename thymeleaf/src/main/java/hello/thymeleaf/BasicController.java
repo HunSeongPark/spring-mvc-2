95039fc472e2d9c5938ad85dc284e4db3016f0c5
Hunseong <phunseong@gmail.com>
2022-04-03 17:39:40 +0900

    thymeleaf-URL 링크

--- thymeleaf/src/main/java/hello/thymeleaf/BasicController.java
+++ thymeleaf/src/main/java/hello/thymeleaf/BasicController.java
@@ -57,6 +57,15 @@ public class BasicController {
         session.setAttribute("sessionData", "Hello Session");
         return "basic/basic-objects";
     }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
