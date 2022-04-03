a27a7ebf788b7f25f5ee128b296f7eee0656f0ac
Hunseong <phunseong@gmail.com>
2022-04-04 06:12:14 +0900

    BasicErrorController가 제공하는 기본 API Exception 확인

---
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,32 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Hunseong on 2022/04/04
+ */
+@Slf4j
+@RestController
+public class ApiExceptionController {
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable String id) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+
+        return new MemberDto(id, "hello" + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
