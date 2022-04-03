5bf140e45d5e19f3fa392ab5f43cef1368af72a7
Hunseong <phunseong@gmail.com>
2022-04-04 04:46:17 +0900

    @SessionAttribute를 통한 세션 조회

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -20,13 +21,13 @@ import static hello.login.web.session.SessionManager.SESSION_COOKIE_NAME;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    
+
 //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(HttpServletRequest request, Model model) {
 
         // 세션 조회
@@ -38,6 +39,20 @@ public class HomeController {
         Member loginMember = (Member) session.getAttribute(LOGIN_MEMBER);
 
         // 세션에 회원 데이터가 없으면 null
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginSpring(
+            @SessionAttribute(name = LOGIN_MEMBER, required = false) Member loginMember,
+            Model model
+    ) {
+        // 세션이 없거나 세션 내 회원 데이터가 없으면 null
         if (loginMember == null) {
             return "home";
         }
