d1f953845944cc84742c9ea126d884f3ea77c82e
Hunseong <phunseong@gmail.com>
2022-04-04 07:10:00 +0900

    converter project init: :q

---
+++ converter/src/main/java/hello/converter/ConverterApplication.java
@@ -0,0 +1,13 @@
+package hello.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverterApplication.class, args);
+	}
+
+}
---
+++ converter/src/test/java/hello/converter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
